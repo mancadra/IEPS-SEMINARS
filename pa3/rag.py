ad71e484c20665cc445dfa3448fcf2f0bb03fb12
blurink <jan.novak.domijan@gmail.com>
2025-04-20 10:26:11 +0200

    Integrated pa2 into pa3.

--- pa3/rag.py
+++ pa3/rag.py
@@ -1,8 +1,16 @@
 import dspy
+import db_handler
+import helper
 
-# Integration point for PA2
+helper = helper.Helper()
+config = helper.get_config()
+db = db_handler.DbHandler()
+similarity_metric = config['MODEL']['SIMILARITY_METRIC']
+model_name = config['MODEL']['LLM']
+table_name = 'crawldb.page_segment'
+
 def query_database(query):
-    return "banana"
+    return db.query_similarity(query, table_name, similarity_metric)
 
 class RAG(dspy.Module):
     def __init__(self):
@@ -12,7 +20,7 @@ class RAG(dspy.Module):
         context = query_database(question)
         return self.respond(context=context, question=question)
 
-lm = dspy.LM('ollama_chat/llama3.2:1b', api_base='http://localhost:11434', api_key='')
+lm = dspy.LM(f"ollama_chat/{model_name}", api_base='http://localhost:11434', api_key='')
 dspy.configure(lm=lm)
 
 naive_rag = dspy.ChainOfThought('question -> answer: str')
